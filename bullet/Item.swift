a9dd83d7534fa306e07860f6069170490b8ba5cb
Mees <git@amees.me>
2025-12-01 19:55:00 +0100

    chore: project initiation

---
+++ bullet/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  bullet
+//
+//  Created by Mees on 01/12/2025.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
